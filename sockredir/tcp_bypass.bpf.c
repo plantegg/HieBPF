a8c1d43c0c4ac247320c19bed04601c344a68e96
test <test@ren>
2024-09-13 11:31:40 +0800

    将两个 bpf 合并成一个，解决掉 sock_map 共享的问题，最终完全可用并且更简单了

--- sockredir/tcp_bypass.bpf.c
+++ sockredir/tcp_bypass.bpf.c
@@ -1,6 +1,6 @@
 #include <linux/bpf.h>
 
-#include "tcp_bypass_ops.h"
+#include "tcp_bypass.h"
 
 /* extract the key that identifies the destination socket in the sock_ops_map */
 static inline
@@ -17,6 +17,62 @@ void sk_msg_extract4_key(struct sk_msg_md *msg,
 	//key->sport = (bpf_htonl(msg->remote_port) >> 16);
 }
 
+/*
+ * extract the key identifying the socket source of the TCP event 
+ */
+static inline
+void sk_extractv4_key(struct bpf_sock_ops *ops,
+	struct sock_key *key)
+{
+	// keep ip and port in network byte order
+	key->dip4 = ops->remote_ip4;
+	key->sip4 = ops->local_ip4;
+	key->family = 1;
+	
+	// local_port is in host byte order, and 
+	// remote_port is in network byte order
+	key->sport = (bpf_htonl(ops->local_port) >> 16);
+	key->dport = FORCE_READ(ops->remote_port) >> 16;
+}
+
+static inline
+void bpf_sock_ops_ipv4(struct bpf_sock_ops *skops)
+{
+	struct sock_key key = {};
+
+	//skip port 10000, 10000端口作为正常流量进行对比
+	if(bpf_ntohl(skops->remote_port)==10000 || skops->local_port==10000)
+		return ;
+	
+	sk_extractv4_key(skops, &key);
+
+	// insert the source socket in the sock_ops_map
+	//定义在 include/uapi/linux/bpf.h 中,OS 提供的能力
+	//bpf.h 种的函数被libbpf 重新在 bpf_helper_defs.h 中封装
+	int ret = sock_hash_update(skops, &sock_ops_map, &key, BPF_NOEXIST);
+	printk("<<< ipv4 op = %d, port %d --> %d\n", 
+		skops->op, skops->local_port, bpf_ntohl(skops->remote_port));
+	if (ret != 0) {
+		printk("FAILED: sock_hash_update ret: %d\n", ret);
+	}
+}
+
+SEC("sockops")
+int bpf_sockops_v4(struct bpf_sock_ops *skops)
+{
+	switch (skops->op) {
+        case BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB:
+        case BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB:
+		if (skops->family == 2) { //AF_INET
+                        bpf_sock_ops_ipv4(skops);
+		}
+                break;
+        default:
+                break;
+        }
+	return 0;
+}
+
 SEC("sk_msg")
 int bpf_tcpip_bypass(struct sk_msg_md *msg)
 {
@@ -25,9 +81,10 @@ int bpf_tcpip_bypass(struct sk_msg_md *msg)
     sk_msg_extract4_key(msg, &key);
     //定义在 include/uapi/linux/bpf.h 中,所有内核提供的函数都在这里
     ret=msg_redirect_hash(msg, &sock_ops_map, &key, BPF_F_INGRESS);
-    printk("redir msg from %d to %d and ret: %d\n", msg->local_port, 
-		(bpf_htonl(msg->remote_port)), ret);
+    printk("redir msg from %d to %d and ret: %d\n", msg->local_port,
+                (bpf_htonl(msg->remote_port)), ret);
     return SK_PASS;
 }
 
 char ____license[] __section("license") = "GPL";
+int _version __section("version") = 1;
---
+++ sockredir/tcp_bypass.c
@@ -0,0 +1,61 @@
+#include <bpf/libbpf.h>
+#include <fcntl.h>
+
+#include "tcp_bypass.skel.h"
+
+int main(int argc, char **argv)
+{
+	struct tcp_bypass_bpf *skel;
+	int err, prog_fd, cgfd;
+	int sock_map_id;
+
+	err=0;
+
+	//cgroup mount
+	cgfd = open("/tmp/unified", O_RDONLY);
+	
+	if (cgfd < 0) {
+		fprintf(stderr, "ERROR: get cgroup %s fd failed\n", "/tmp/unified/");
+		return -1;
+	}
+	printf("cgfd = %d\n", cgfd);
+
+	//open and load, create ebpf prog and maps
+	skel = tcp_bypass_bpf__open_and_load();
+        if (!skel) {
+                fprintf(stderr, "Failed to open and load BPF skeleton\n");
+                return 1;
+        }
+
+	fprintf(stdout, "debug attach\n");
+	//attach
+	bpf_program__attach_cgroup(skel->progs.bpf_sockops_v4, cgfd);	
+	
+	sock_map_id = bpf_map__fd(skel->maps.sock_ops_map);
+
+	fprintf(stdout, "debug map id: %d\n", sock_map_id);
+	
+	prog_fd = bpf_program__fd(skel->progs.bpf_tcpip_bypass);
+
+	fprintf(stdout, "debug attach skeleton prog_fd:%d, map_id:%d\n", prog_fd, sock_map_id);
+	err = bpf_prog_attach(prog_fd, sock_map_id, BPF_SK_MSG_VERDICT, 0);
+	
+	fprintf(stdout, "end debug attach skeleton\n");
+	if (!prog_fd) {
+        	fprintf(stderr, "Failed to attach FD\n");
+    		err=-1;
+	}
+	if (!err) {
+        	fprintf(stderr, "Failed to attach FD2:%d\n", err);
+    		err=-2;
+	}
+	
+        fprintf(stderr, "sleep\n");
+	sleep(10000);
+
+cleanup:
+        tcp_bypass_bpf__destroy(skel);
+        return -err;
+
+}
+
